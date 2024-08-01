2cec345a5cd44798c830ef0b8c333b442af6a181
Deepen Patel <deepenpatel19@gmail.com>
2024-08-01 21:46:35 +0530

    configure env

--- core/settings.go
+++ core/settings.go
@@ -12,53 +12,33 @@ var Config schemas.ProjectConfiguration
 
 func ReadEnvFile() {
 	environment := os.Getenv("ENVIRONMENT")
-	if environment == "production" {
-		fmt.Println("production environment", os.Getenv("DATA"))
-		err := json.Unmarshal([]byte(os.Getenv("DATA")), &Config)
-		if err != nil {
-			fmt.Println("Error while reading env data into struct :: ", err)
-			os.Exit(1)
-		}
-	} else {
-		currentWorkingDirectory, err := os.Getwd()
-		if err != nil {
-			fmt.Println("Error while getting current working directory :: ", err)
-			os.Exit(1)
-		}
-		fmt.Println(currentWorkingDirectory)
-		fileName := currentWorkingDirectory + "/.env"
-
-		file, err := os.ReadFile(fileName)
-		if err != nil {
-			fmt.Println("Error while reading env file :: ", err)
-			os.Exit(1)
-		}
-
-		err = json.Unmarshal(file, &Config)
-		if err != nil {
-			fmt.Println("Error while converting env file data into struct :: ", err)
-			os.Exit(1)
-		}
+	currentWorkingDirectory, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error while getting current working directory :: ", err)
+		os.Exit(1)
 	}
-	// currentWorkingDirectory, err := os.Getwd()
-	// if err != nil {
-	// 	fmt.Println("Error while getting current working directory :: ", err)
-	// 	os.Exit(1)
-	// }
-	// fmt.Println(currentWorkingDirectory)
-	// fileName := currentWorkingDirectory + "/.env"
+	fmt.Println(currentWorkingDirectory)
+	fileName := currentWorkingDirectory + "/.env"
 
-	// file, err := os.ReadFile(fileName)
-	// if err != nil {
-	// 	fmt.Println("Error while reading env file :: ", err)
-	// 	os.Exit(1)
-	// }
+	if environment == "production" {
+		err := os.WriteFile(fileName, []byte(os.Getenv("DATA")), 0755)
+		if err != nil {
+			fmt.Println("unable to write file: %w", err)
+		}
+		fmt.Println("production environment", os.Getenv("DATA"))
+	}
 
-	// err = json.Unmarshal(file, &Config)
-	// if err != nil {
-	// 	fmt.Println("Error while converting env file data into struct :: ", err)
-	// 	os.Exit(1)
-	// }
+	file, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error while reading env file :: ", err)
+		os.Exit(1)
+	}
+
+	err = json.Unmarshal(file, &Config)
+	if err != nil {
+		fmt.Println("Error while converting env file data into struct :: ", err)
+		os.Exit(1)
+	}
 
 	Config.DBString = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
 		Config.DBConfig.DBUser,
